8278554a9bf9690e7c0a4de035a518d262edb55f
avship <alexandershipilo@yandex.ru>
2022-07-20 22:38:54 +0300

    задание хард2

--- src/modules/form-validation.js
+++ src/modules/form-validation.js
@@ -11,6 +11,14 @@ const formValidation = () => {
   const phoneInputChecker = (e) => {
     e.target.value = e.target.value.replace(/[^\d\-\(\)\+]/, "");
   };
+  const messageClearer = (e) => {
+    messageFilter(e);
+    e.target.value = e.target.value.trim();
+    e.target.value = e.target.value.replace(/ +/g, " ");
+    e.target.value = e.target.value.replace(/\-+/g, "-");
+    e.target.value =
+      e.target.value.slice(0, 1).toUpperCase() + e.target.value.slice(1);
+  };
   const calcInputs = document.querySelectorAll(".calc-block input");
   const form2Message = document.querySelector("#form2-message");
   const emailInputs = document.querySelectorAll(".form-email");
@@ -20,13 +28,13 @@ const formValidation = () => {
     item.addEventListener("input", digitsFilter);
   });
   form2Message.addEventListener("input", messageFilter);
+  form2Message.addEventListener("blur", messageClearer);
   emailInputs.forEach((item) => {
     item.addEventListener("input", emailInputChecker);
   });
   phoneInputs.forEach((item) => {
     item.addEventListener("input", phoneInputChecker);
   });
-  console.log(calcInputs);
 };
 
 export default formValidation;
